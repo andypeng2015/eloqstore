9f8051b4511e9bf1e0d62235ebcb298f238fc4be
Chen Zhao <ch3nzhao@gmail.com>
2025-12-17 11:57:10 +0800

    add Pin in SeekIndex

--- index_page_manager.cpp
+++ index_page_manager.cpp
@@ -21,8 +21,9 @@
 
 namespace eloqstore
 {
-IndexPageManager::IndexPageManager(AsyncIoManager *io_manager)
-    : io_manager_(io_manager)
+IndexPageManager::IndexPageManager(AsyncIoManager *io_manager,
+                                   MappingArena *mapping_arena)
+    : io_manager_(io_manager), mapping_arena_(mapping_arena)
 {
     active_head_.EnqueNext(&active_tail_);
 }
@@ -232,13 +233,15 @@ void IndexPageManager::UpdateRoot(const TableIdent &tbl_ident,
     RootMeta &meta = tbl_it->second;
     meta.root_id_ = new_meta.root_id_;
     meta.ttl_root_id_ = new_meta.ttl_root_id_;
-    if (meta.mapper_ != nullptr && !Options()->data_append_mode)
+    auto old_mapper = std::move(meta.mapper_);
+    if (old_mapper != nullptr && !Options()->data_append_mode)
     {
         assert(new_meta.mapper_ != nullptr);
-        MappingSnapshot *prev_snapshot = meta.mapper_->GetMapping();
+        MappingSnapshot *prev_snapshot = old_mapper->GetMapping();
         prev_snapshot->next_snapshot_ = new_meta.mapper_->GetMappingSnapshot();
     }
     meta.mapper_ = std::move(new_meta.mapper_);
+    old_mapper.reset();
     meta.manifest_size_ = new_meta.manifest_size_;
     meta.next_expire_ts_ = new_meta.next_expire_ts_;
     meta.compression_ = std::move(new_meta.compression_);
@@ -470,42 +473,6 @@ void IndexPageManager::FinishIo(MappingSnapshot *mapping,
 KvError IndexPageManager::SeekIndex(MappingSnapshot *mapping,
                                     PageId page_id,
                                     std::string_view key,
-                                    std::span<PageId> results,
-                                    size_t &result_size_dst)
-{
-    auto [node, err] = FindPage(mapping, page_id);
-    CHECK_KV_ERR(err);
-    IndexPageIter idx_it{node, Options()};
-    idx_it.Seek(key);
-    PageId child_id = idx_it.GetPageId();
-    if (!node->IsPointingToLeaf())
-    {
-        return SeekIndex(mapping, child_id, key, results, result_size_dst);
-    }
-
-    size_t result_size = 0;
-    const size_t result_limit = results.size();
-    if (child_id != MaxPageId && result_size < result_limit)
-    {
-        results[result_size++] = child_id;
-    }
-
-    while (result_size < result_limit && idx_it.HasNext() && idx_it.Next())
-    {
-        child_id = idx_it.GetPageId();
-        if (child_id == MaxPageId)
-        {
-            break;
-        }
-        results[result_size++] = child_id;
-    }
-    result_size_dst = result_size;
-    return KvError::NoError;
-}
-
-KvError IndexPageManager::SeekIndex(MappingSnapshot *mapping,
-                                    PageId page_id,
-                                    std::string_view key,
                                     PageId &result)
 {
     auto [node, err] = FindPage(mapping, page_id);
@@ -520,6 +487,7 @@ KvError IndexPageManager::SeekIndex(MappingSnapshot *mapping,
     }
     else
     {
+        ThdTask()->YieldToNextRound();
         return SeekIndex(mapping, child_id, key, result);
     }
 }
@@ -532,5 +500,10 @@ const KvOptions *IndexPageManager::Options() const
 AsyncIoManager *IndexPageManager::IoMgr() const
 {
     return io_manager_;
+}
+
+MappingArena *IndexPageManager::MapperArena() const
+{
+    return mapping_arena_;
 }
 }  // namespace eloqstore
